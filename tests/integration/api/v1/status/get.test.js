aade9306db3d5b091e59370b154e49628f198356
Gabriel Marques <59850744+gabrielmarquestech@users.noreply.github.com>
2024-06-14 20:29:02 +0000

    refactor database version test assertion

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,7 +9,7 @@ test("GET to api/v1/status returns 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toBe("16.1");
+  expect(responseBody.dependencies.database.version).toEqual("16.1");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
